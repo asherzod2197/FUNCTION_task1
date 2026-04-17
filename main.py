f7f7329acecdba583329f0de2bb391d9b31d6bd2
Ataxanov <asherzod2197@gmail.com>
2026-04-17 17:09:18 +0500

    Add multiple functions with greeting messages

---
+++ main.py
@@ -0,0 +1,50 @@
+# 1
+def salomlashuv():
+    print("Salom, dunyo!")
+
+# 2
+def raqam_korsatish():
+    print(100)
+
+# 3
+def motivatsiya():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+# 4
+def takrorlanuvchi_matn():
+    print("Python " * 3)
+
+# 5
+def oddiy_hisob():
+    print(5 + 7)
+
+# 6
+def vaqt_xabari():
+    print("Hozir tong!")
+
+# 7
+def fikr():
+    print("O'rganish - kelajak!")
+
+# 8
+def boshlanish_xabari():
+    print("Dastur ishga tushdi!")
+
+# 9
+def tasodifiy_salom():
+    print("Bugun yaxshi kun!")
+
+# 10
+def oddiy_chiqarish():
+    print("Xush kelibsiz!")
+
+salomlashuv()
+raqam_korsatish()
+motivatsiya()
+takrorlanuvchi_matn()
+oddiy_hisob()
+vaqt_xabari()
+fikr()
+boshlanish_xabari()
+tasodifiy_salom()
+oddiy_chiqarish()
